8a59fba8c7a353daa6a744f34cf3ff8d28afa1d6
HERMINDER SINGH <hermindersingh12345@gmail.com>
2019-06-24 17:51:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sunshine/bottomnavigation/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.sunshine.bottomnavigation
+
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.nav_view
+
+class MainActivity : AppCompatActivity(),
+    BottomNavigationView.OnNavigationItemSelectedListener {
+    override fun onNavigationItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        R.id.navigation_home -> {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, BlankFragment()).commit()
+            true
+        }
+        R.id.navigation_dashboard -> {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, BlankFragment2()).commit()
+            true
+        }
+        R.id.navigation_notifications -> {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, BlankFragment3()).commit()
+            true
+        }
+        else -> false
+
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        nav_view.setOnNavigationItemSelectedListener(this)
+    }
+}
